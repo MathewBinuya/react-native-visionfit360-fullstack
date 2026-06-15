7ee517ad3124b109c7e5d912253913c46ec72eb9
CodewithPixie <xzocantsleep@gmail.com>
2026-06-15 19:15:15 +0800

    Point mobile and admin to live Render backend

--- mobile/lib/api.js
+++ mobile/lib/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.0.196:3000/api";
-// export const API_URL = "https://visionfit360.onrender.com/api";
+export const API_URL = "https://visionfit-backend-cwxi.onrender.com/api"; 
+// export const API_URL = "http://192.168.0.196:3000/api";
